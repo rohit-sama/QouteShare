69be3febc024e25046f7037393b778c1a96652df
rohit-sama <rohit9804singh@gmail.com>
2023-07-21 00:10:27 +0530

    GN

---
+++ app/update-qoute/page.jsx
@@ -0,0 +1,60 @@
+"use client"
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter,useSearchParams } from 'next/navigation';
+import Form from '@components/Form';
+
+
+const EditQoute = () => {
+const router = useRouter();
+const {data : session} = useSession();
+
+
+const [submitting, setSubmitting] = useState(false);
+const [post, setPost] = useState({
+    qoute: "",
+    image: "",
+    tag: "",
+})
+
+useEffect (() =>{
+
+},[qouteId])
+
+const createQoute = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/qoute/new',{
+        method: "POST",
+        body:JSON.stringify({
+          qoute: post.qoute,
+          image: post.image,
+          userId: session?.user.id,
+          tag:post.tag
+        })
+      })
+
+      if(response.ok){
+        router.push("/") 
+      }
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setSubmitting(false);
+    }
+}
+
+  return (
+    <Form
+     type= "Create"
+     post = {post}
+     setPost = {setPost}
+     submitting = {submitting}
+     handleSubmit = {createQoute}
+     />
+  )
+}
+
+export default EditQoute;
